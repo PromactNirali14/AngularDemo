e88fbe12cafd4707f72b04d70957cf8e023b75b8
PromactNirali14 <nikyit9@gmail.com>
2014-12-18 17:24:01 +0530

    Validation

--- Nirali/CrowdFunding - Copy/CrowdFunding/Models/ProjectUpdates.cs
+++ Nirali/CrowdFunding - Copy/CrowdFunding/Models/ProjectUpdates.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,9 @@ namespace CrowdFunding.Models
     public class ProjectUpdates
     {
         public int Id { get; set; }
+        [ForeignKey("ProjectInfo")]
         public int ProjectInfoId { get; set; }
+        public virtual ProjectInfo ProjectInfo { get; set; }
         public int UserInfoId { get; set; }
         [Required]
         [StringLength(140)]
--- Nirali/CrowdFunding - Copy/CrowdFunding/Models/ProjectUpdatesComments.cs
+++ Nirali/CrowdFunding - Copy/CrowdFunding/Models/ProjectUpdatesComments.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,9 @@ namespace CrowdFunding.Models
     public class ProjectUpdatesComments
     {
         public int Id { get; set; }
+        [ForeignKey("ProjectUpdates")]
         public int ProjectUpdatesId { get; set; }
+        public virtual ProjectUpdates ProjectUpdates { get; set; }
         public int UserInfoId { get; set; }
         [Required]
         public string Comment { get; set; }
