ad2092cff7b6a6f24fe0623cc548a81f0801da32
PromactNirali14 <nikyit9@gmail.com>
2014-12-27 16:50:23 +0530

    Demo Of AngularJs

---
+++ Demo/DemoAngularJs/DemoAngularJs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoAngularJs.Startup))]
+namespace DemoAngularJs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
